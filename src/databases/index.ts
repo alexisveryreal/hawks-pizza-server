97a76b6ca8bd5d912d3980d277ea07e6d0fc887c
alexisveryreal <alexmaple20@gmail.com>
2022-03-19 15:21:49 -0700

    fix mongo url i belib

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,11 +1,11 @@
-import { MONGO_URL, MONGOUSER, MONGOPASSWORD, MONGOHOST, MONGOPORT } from '@/config';
+import { MONGO_URL } from '@/config';
 
 if (!MONGO_URL) {
   throw new Error('Please add the MONGO_URL environment variable');
 }
 
 export const dbConnection = {
-  url: `mongodb+srv://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`,
+  url: MONGO_URL,
   options: {
     dbName: 'test',
   },
