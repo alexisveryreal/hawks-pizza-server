139a98570d70c2a6cad39893d0d82804e0e7f186
alexisveryreal <alexmaple20@gmail.com>
2022-03-19 13:57:52 -0700

    * change to simpler version and using npm with swrc

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
-import "./lib/db";
-import express from "express";
-import countryRoutes from "./routes/country";
+import express from 'express';
 
+const PORT = 3000;
 const app = express();
-const port = process.env.PORT || 3333;
 
-app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
 
-app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+app.get('/test', (req, res) => {
+  res.status(200).send('Hello World');
 });
 
-app.use("/countries", countryRoutes);
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
